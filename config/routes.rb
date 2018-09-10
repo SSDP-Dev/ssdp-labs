8f2e3915edecb705990d09a3cbe8351885e720ce
Tyler <tyler@ogdenstudios.xyz>
2018-09-10 15:34:57 -0600

    add ckeditor on edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root                       to: 'static_pages#index'
   get  'posts',              to: 'posts#index'
   get  '/posts/new',         to: 'posts#new'
   get  '/posts/edit',        to: 'posts#edit'
@@ -10,5 +11,4 @@ Rails.application.routes.draw do
   get  '/pages/edit',        to: 'pages#edit'
   post '/pages/write',       to: 'pages#write'
   get  '/pages/destroy',     to: 'pages#destroy'
-  root to: 'static_pages#index'
 end
