90b5098730b60fc9f53af440acd8758307399f80
Tyler <tyler@ogdenstudios.xyz>
2018-09-20 20:14:31 -0600

    add page model and run db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180919223851) do
+ActiveRecord::Schema.define(version: 20180921021419) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.string "slug"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
